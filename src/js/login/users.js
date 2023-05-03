f77e97f198230863706642bd8268294a29e452d0
Kinnitchi <igorivieira97@gmail.com>
2023-05-02 22:42:31 -0300

    ADJUST: removed function in script users.js

---
+++ src/js/login/users.js
@@ -0,0 +1,25 @@
+const users = [{
+      id: 1,
+      login: 'igor.oliveira',
+      password: 'teste',
+      email: 'igor.oliveira@example.com',
+      name: 'Igor Oliveira',
+      role: 'admin'
+   },
+   {
+      id: 2,
+      login: 'igor.vieira',
+      password: 'teste',
+      email: 'igor.viera@example.com',
+      name: 'Igor Vieira',
+      role: 'user'
+   },
+   {
+      id: 3,
+      login: 'admin',
+      password: 'admin',
+      email: 'admin@example.com',
+      name: 'Administrator',
+      role: 'admin'
+   }
+];
